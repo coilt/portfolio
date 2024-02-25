d437fa62e63eb51923bb9d44e0d0747066a2baa8
coilt <cameraboi@gmail.com>
2024-02-25 19:03:30 +0700

    Functional card scroll

--- src/app/components/scrollcards/ScrollCards.js
+++ src/app/components/scrollcards/ScrollCards.js
@@ -1,16 +1,28 @@
+'use client';
+import React, { useRef } from 'react';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import styles from './styles.scrollcards.module.scss'; 
 import Image from 'next/image';
 import Card1 from './card_01.jpg';
 
 
 export default function ScrollCards() {
+  const container = useRef(null);
+const { scrollYProgress } = useScroll({
+  target: container,
+  offset: ['start start', 'end end']
+})
+
+const cardFlip = useTransform(scrollYProgress, [1, 0], [0.8,1] ) 
+ 
+
   return (
 
-<div className={styles.container}>
+<div ref={container} className={styles.container}>
 <div className={styles.scrollCard}> 
 
 <div className={styles.card01}>
-<div className={styles.cardContainer}>
+<motion.div style={{scale: cardFlip}} className={styles.cardContainer}>
   
 <Image 
 
@@ -21,7 +33,7 @@ alt='card'
 
 />
 
-  </div>
+  </motion.div>
 
 </div>
 
