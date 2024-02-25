aa13db62b5f31284db772bee470557474912b026
coilt <cameraboi@gmail.com>
2024-02-26 02:34:27 +0700

    Add double card scroll with a bug

--- src/app/components/flipcards/FlipCards.jsx
+++ src/app/components/flipcards/FlipCards.jsx
@@ -9,39 +9,70 @@ import Card2 from './card_02.jpg'
 import './PortfolioComponent.css'
 
 export default function FlipCard() {
-  const cardRef = useRef(null)
-  const card1 = useRef(null) // Corrected to useRef(null)
-  const portfolioRef = useRef(null)
+  const portfolio1Ref = useRef(null)
+  const portfolio2Ref = useRef(null)
 
   useGSAP(() => {
     gsap.registerPlugin(ScrollTrigger)
 
-    gsap.set(portfolioRef.current, { visibility: "visible", xPercent: 100, yPercent: 100, filter: "blur(0px)" });    
-    gsap.to(portfolioRef.current, {
-      scale: 0.4,
+    gsap.set(portfolio1Ref.current, {
+      visibility: 'visible',
+      xPercent: 100,
+      yPercent: 100,
+      filter: 'blur(0px)',
+    })
+    gsap.to(portfolio1Ref.current, {
+      scale: 0.8,
       x: '-1%',
-      filter: 'blur(30px)', // Apply blur effect
+      filter: 'blur(20px)', // Apply blur effect
       scrollTrigger: {
-        trigger: portfolioRef.current,
-        start: 'top 10%',
+        trigger: portfolio1Ref.current,
+        start: 'top 5%',
         end: '+=2000',
-        markers: true,
+        markers: false,
         pin: true,
+        pinType: 'fixed',
         scrub: true,
+        ease: "power1.out",
+      },
+    })
+
+    gsap.set(portfolio2Ref.current, {
+      visibility: 'visible',
+      xPercent: 100,
+      yPercent: 100,
+      filter: 'blur(0px)',
+    })
+    gsap.to(portfolio2Ref.current, {
+      scale: 0.8,
+      x: '-1%',
+      filter: 'blur(20px)', // Apply blur effect
+      scrollTrigger: {
+        trigger: portfolio2Ref.current,
+        start: 'top 5%',
+        end: '+=2000',
+        markers: false,
+        pin: true,
+        pinType: 'fixed',
+
+        scrub: 1,
+        ease: "power1.out",
+         
       },
     })
   }, [])
 
   return (
     <>
-    <div className='wrapper'>
-      <div
-        ref={portfolioRef}
-        className='portfolio'
-     
-      >
-        <Image src={Card1} alt='Portfolio' fill />
+      <div className='wrapper'>
+        <div ref={portfolio1Ref} className='portfolio'>
+          <Image src={Card1} alt='Portfolio' fill />
+        </div>
       </div>
+      <div className='wrapper2'>
+        <div ref={portfolio2Ref} className='portfolio2'>
+          <Image src={Card2} alt='Portfolio' fill />
+        </div>
       </div>
     </>
   )
